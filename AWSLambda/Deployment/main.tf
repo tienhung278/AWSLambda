588416216d153c36738def353dd071513c8898c4
Hung Pham <hung.pham@doctoranywhere.com>
2022-07-04 20:02:54 +0700

    reconfig vpc

--- AWSLambda/Deployment/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "mtc_vpc" {
-  cidr_block = "10.123.0.0/16"
-  enable_dns_hostnames = true
-  
-  tags = {
-    Name = "dev"
-  }
-}
---
+++ AWSLambda/Deployment/vpc.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "first-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "dev"
+  }
+}
+
+resource "aws_subnet" "first-subnet" {
+  vpc_id = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "dev-subnet"
+  }
+}
