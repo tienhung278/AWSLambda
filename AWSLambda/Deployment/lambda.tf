8297270725f2335302a04d64a7ea4b74715ed714
Hung Pham <hung.pham@doctoranywhere.com>
2022-07-04 15:34:36 +0700

    Deploy lambda func to AWS using terraform

---
+++ AWSLambda/Deployment/lambda.tf
@@ -0,0 +1,20 @@
+locals {
+    lambda_zip_location = "outputs/AWSLambda.zip"
+}
+
+data "archive_file" "AWSLambda" {
+  type        = "zip"
+  source_file = "../bin/Debug/net6.0/"
+  output_path = local.lambda_zip_location
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = local.lambda_zip_location
+  function_name = "FunctionHandler"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "AWSLambda::AWSLambda.Function::FunctionHandler"
+
+  source_code_hash = filebase64sha256(lambda_zip_location)
+
+  runtime = "dotnet6"
+}
