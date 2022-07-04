f77c8cedb91ae1b64729fd55e3df82b9b2e38f29
Hung Pham <70580007+tienhung278@users.noreply.github.com>
2022-07-04 22:46:27 +0700

    Update vpc.tf

--- AWSLambda/Deployment/vpc.tf
+++ AWSLambda/Deployment/vpc.tf
@@ -9,6 +9,7 @@ resource "aws_vpc" "first-vpc" {
 resource "aws_subnet" "first-subnet" {
   vpc_id = aws_vpc.first-vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-southeast-2"
 
   tags = {
     Name = "dev-subnet"
